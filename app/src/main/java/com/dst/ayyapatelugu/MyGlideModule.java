6cc43c6ba9f5839aa1a153ab4c8fc732ab5649b9
hp <anji.pingali@gmail.com>
2024-11-28 15:38:05 +0530

    all files are commited

--- app/src/main/java/com/dst/ayyapatelugu/MyGlideModule.java
+++ app/src/main/java/com/dst/ayyapatelugu/MyGlideModule.java
@@ -13,6 +13,7 @@ import com.bumptech.glide.module.AppGlideModule;
 
 import java.io.InputStream;
 
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
@@ -31,6 +32,6 @@ public class MyGlideModule extends AppGlideModule {
                 .build();
 
         // Register OkHttpUrlLoader with Glide
-        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(client));
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory((Call.Factory) client));
     }
 }
